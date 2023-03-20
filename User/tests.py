9425d26a153a3eca31f9ddca66a175fda61917d8
沈佳茗 <2021010745@secoder.net>
2023-03-20 23:38:26 +0800

    Fix: add one test, try to pass unit-test

--- User/tests.py
+++ User/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+import random
+from django.test import TestCase, Client
 
 # Create your tests here.
+class UserTests(TestCase):
+    def test_for_unit(self):
+        self.assertNotEqual(0, 1)
