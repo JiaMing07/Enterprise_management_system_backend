8a11a7441d1511ff520f3eb551f152706f4d72a7
沈佳茗 <2021010745@secoder.net>
2023-03-22 22:01:39 +0800

    Fix: correct commands of migrate in start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-python3 manage.py makemigrations board
+python3 manage.py makemigrations Department
+python3 manage.py makemigrations User
 python3 manage.py migrate
 
 # TODO Start: [Student] Run with uWSGI instead
-# python3 manage.py runserver 80
+# python3 manage.py runserver 8000
 uwsgi --module=DjangoHW.wsgi:application \
     --env DJANGO_SETTINGS_MODULE=DjangoHW.settings \
     --master \
