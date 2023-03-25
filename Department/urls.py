a75bacb30acef4f87a22ad1456da071db660072f
沈佳茗 <2021010745@secoder.net>
2023-03-26 03:36:38 +0800

    Feat: add department/add api(#9)

--- Department/urls.py
+++ Department/urls.py
@@ -4,5 +4,5 @@ import Department.views as views
 urlpatterns = [
     path('startup', views.startup),
     path('entity/add', views.add_entity),
-    # path('department/add', views.add_department),
+    path('department/add', views.add_department),
 ]
--- Department/views.py
+++ Department/views.py
@@ -27,3 +27,26 @@ def add_entity(req: HttpRequest):
         entity.save()
         return request_success() 
     return BAD_METHOD
+
+@CheckRequire
+def add_department(req: HttpRequest):
+    if req.method == 'POST':
+        body = json.loads(req.body.decode("utf-8"))
+        entity_name, department_name, parent_name = get_args(body, ["entity", "department", "parent"], ["string", "string", "string"])
+        entity = Entity.objects.filter(name = entity_name).first()
+        parent = Department.objects.filter(name=parent_name).first()
+        if parent_name == "":
+            parent = Department.objects.filter(id=1).first()
+        if entity is None:
+            return request_failed(1, "企业实体不存在", status_code=403)
+        if parent is None:
+            return request_failed(1, "父部门不存在", status_code=403)
+        department = Department.objects.filter(name=department_name).first()
+        if department is not None:
+            print(department.name)
+            return request_failed(1, "该部门已存在", status_code=403)
+        department = Department(name=department_name, entity=entity, parent=parent)
+        department.save()
+        return request_success()
+
+    return BAD_METHOD
