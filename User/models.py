760cf7522ac545eac3a8a286369495dfa5d0b954
沈佳茗 <2021010745@secoder.net>
2023-03-28 12:04:35 +0800

    Fix: delete debug message of test(#7)

--- User/models.py
+++ User/models.py
@@ -25,12 +25,8 @@ class User(models.Model):
 
     def check_password(self, pwd):
         if pwd == self.password:
-            print("True")
             return True
         else:
-            print("False")
-            print(pwd)
-            print(self.password)
             return False
     
     def generate_token(self):
