d4eb88f275bf964bb5ee35c226fb3ee0f9813f49
Camellia_TT <wangchuy21@mails.tsinghua.edu.cn>
2023-04-08 16:43:14 +0800

    Feature: add frac for api asset/attribute/add(#24)

--- Asset/urls.py
+++ Asset/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 import Asset.views as views
 
 urlpatterns = [
-    
+    path('attribute/add', views.attribute_add),
 ]
--- Asset/views.py
+++ Asset/views.py
@@ -10,3 +10,19 @@ from utils.utils_checklength import checklength
 from utils.utils_checkauthority import CheckAuthority, CheckToken
 
 # Create your views here.
+
+@CheckRequire    
+def attribute_add(req: HttpRequest):
+    if req.method == 'POST':
+        name = json.loads(req.body.decode("utf-8")).get('name')
+
+        # check format
+        checklength(name, 0, 50, "atrribute_name")
+
+        # add in Attribute
+
+        ## filter whether exist
+
+        ## save
+    else:
+        return BAD_METHOD
