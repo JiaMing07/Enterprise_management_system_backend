9d58330c680b3e2151969d143ca6f99da31e075d
Camellia_TT <wangchuy21@mails.tsinghua.edu.cn>
2023-04-10 12:01:41 +0800

    Test: add test for token-check attribute/add api

--- Asset/tests.py
+++ Asset/tests.py
@@ -4,6 +4,9 @@ from User.models import User, Menu
 from Department.models import Department, Entity
 from Asset.models import Attribute, Asset, AssetAttribute, AssetCategory
 
+from http import cookies
+import hashlib
+
 # Create your tests here.
 
 # Test for Attribute
@@ -15,6 +18,14 @@ class AttributeTests(TestCase):
     def setUp(self):
         ent = Entity.objects.create(id=1, name='ent')
         attri_0 = Attribute.objects.create(id=1, name="attri_0", entity=ent)
+        dep_ent = Department.objects.create(id=1, name='ent', entity=ent)
+        dep = Department.objects.create(id=2, name='dep', entity=ent)
+        password='123'
+        md5 = hashlib.md5()
+        md5.update(password.encode('utf-8'))
+        pwd = md5.hexdigest()
+        User.objects.create(username='Alice', password=pwd, department=dep, entity=ent)
+        User.objects.create(username='test_attribute', password=pwd, department=dep, entity=ent)
 
     def post_attribute_add(self, name):
         payload = {
@@ -25,6 +36,16 @@ class AttributeTests(TestCase):
         return self.client.post("/asset/attribute/add", data=payload, content_type="application/json")
 
     def test_attribute_add(self):
+
+        # token
+        user = User.objects.filter(username='test_attribute').first()
+        user.token = user.generate_token()
+        user.system_super, user.entity_super, user.asset_super = user.set_authen("asset_super")
+        user.save()
+        Token = user.token
+        c = cookies.SimpleCookie()
+        c['token'] = Token
+        self.client.cookies = c
 
         # success
         name = "attri_1"
