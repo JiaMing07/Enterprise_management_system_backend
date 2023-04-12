bf4e4ead6433839ba2643c8d148d032173fdbf9e
沈佳茗 <2021010745@secoder.net>
2023-04-12 21:23:40 +0800

    Feat: change user/menu 'GET' return(#28)

--- User/tests.py
+++ User/tests.py
@@ -632,6 +632,7 @@ class UserTests(TestCase):
         authority = 'entity_super/asset_super'
 
         res = self.post_user_menu(first, second, url, authority)
+        print(res.json()['info'])
         self.assertEqual(res.json()['code'], 0)
         # test repeat first
         first = 'f_1'
@@ -731,6 +732,13 @@ class UserTests(TestCase):
         res = self.get_user_menu()
 
         self.assertEqual(res.json()['code'], 0)
+        get_list = res.json()['menu']
+        menu_list = Menu.objects.filter(entity_show=True)
+        index=0
+        for menu in get_list:
+            self.assertEqual(menu['first'], menu_list[index].first)
+            self.assertEqual(menu['second'], menu_list[index].second)
+            index+=1
 
         # check entity_super
         authority = 'staff'
@@ -739,6 +747,13 @@ class UserTests(TestCase):
         res = self.get_user_menu()
 
         self.assertEqual(res.json()['code'], 0)
+        get_list = res.json()['menu']
+        menu_list = Menu.objects.filter(staff_show=True)
+        index=0
+        for menu in get_list:
+            self.assertEqual(menu['first'], menu_list[index].first)
+            self.assertEqual(menu['second'], menu_list[index].second)
+            index+=1
 
         # check asset_super
         authority = 'asset_super'
@@ -747,6 +762,13 @@ class UserTests(TestCase):
         res = self.get_user_menu()
 
         self.assertEqual(res.json()['code'], 0)
+        get_list = res.json()['menu']
+        menu_list = Menu.objects.filter(asset_show=True)
+        index=0
+        for menu in get_list:
+            self.assertEqual(menu['first'], menu_list[index].first)
+            self.assertEqual(menu['second'], menu_list[index].second)
+            index += 1
             
         # method delete
         # add 2 menu
@@ -774,16 +796,9 @@ class UserTests(TestCase):
         self.assertEqual(res.json()['code'], 1)
         self.assertEqual(res.json()['info'], '一级菜单不存在')
 
-        first = 'm1'
-        second = ''
-        res = self.delete_user_menu(first, second)
-        self.assertEqual(res.json()['code'], 3)
-        self.assertEqual(res.json()['info'], '不可删除初始一级菜单')
-
         first = 'f_2'
         second = 's_2'
         res = self.delete_user_menu(first, second)
-        print(res.json()['info'])
         self.assertEqual(res.json()['code'], 2)
         self.assertEqual(res.json()['info'], '二级菜单不存在')
 
@@ -794,10 +809,3 @@ class UserTests(TestCase):
         second = ''
         res = self.delete_user_menu(first, second)
         self.assertEqual(res.json()['code'], 0)
-
-        first = 'm4'
-        second = 's1'
-        res = self.delete_user_menu(first, second)
-        print(res.json()['info'])
-        self.assertEqual(res.json()['code'], 4)
-        self.assertEqual(res.json()['info'], '不可删除初始二级菜单')
