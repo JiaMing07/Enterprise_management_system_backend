JiaMing07/Enterprise_management_system_backend

commit c0139ad25c188807c8435c0251e1b0e7484dc656

Feat: add asset/user api(#45)

Asset/tests.py
@@ -182,6 +182,9 @@ class AttributeTests(TestCase):
     def get_asset_query(self, type, description, attribute):
         return self.client.get(f'/asset/query/{type}/{description}/{attribute}')
     
+    def get_asset_user_query(self):
+        return self.client.get(f'/asset/user')
+    
     # Now start testcases. 
     def test_asset_category_add(self):
         user = User.objects.filter(username='test_user').first()
@@ -1403,3 +1406,17 @@ class AttributeTests(TestCase):
         self.assertTrue(Asset.objects.filter(name='computer').exists())
         self.assertTrue(Asset.objects.filter(name='ent').exists())
         self.assertTrue(Asset.objects.filter(name='keyboard').exists())
+
+    def test_asset_user_query(self):
+        user = User.objects.filter(username='test_user').first()
+        user.token = user.generate_token()
+        user.system_super, user.entity_super, user.asset_super = user.set_authen("entity_super")
+        user.save()
+        Token = user.token
+        c = cookies.SimpleCookie()
+        c['token'] = Token
+        self.client.cookies = c
+
+        res = self.get_asset_user_query()
+        self.assertEqual(res.json()['info'], "Succeed")
+        self.assertEqual(res.json()['code'], 0)

Asset/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('tree', views.asset_tree),
     path('add', views.asset_add),
     path('edit', views.asset_edit),
+    path('user', views.user_query),
     path('add/list', views.asset_add_list),
     path('query/<type>/<description>/<attribute>', views.asset_query),
     path('delete', views.asset_delete),
@@ -20,5 +21,5 @@ urlpatterns = [
     path('attribute', views.asset_attribute),
     path('<assetName>', views.asset_assetName),
     path('category/<category_name>/number', views.asset_category_number),
-    path('attribute/<assetName>', views.asset_attribute_list)
+    path('attribute/<assetName>', views.asset_attribute_list),
 ]

Asset/views.py
@@ -662,9 +662,7 @@ def asset_attribute_list(req: HttpRequest, assetName: any):
         idx = require({"asset": assetName}, "asset", "string", err_msg="Bad param [asset]", err_code=-1)
         checklength(assetName, 0, 50, "asset_name")
         
-        CheckToken(req)
-        token = req.COOKIES['token'] 
-        decoded = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        token, decoded = CheckToken(req)
         user = User.objects.get(username=decoded['username'])
 
         asset = Asset.objects.get(entity=user.entity, name=assetName)
@@ -813,3 +811,18 @@ def asset_query(req: HttpRequest, type: str, description: str, attribute:str):
         return request_success(return_data)
     else:
         return BAD_METHOD
+
+@CheckRequire
+def user_query(req: HttpRequest):
+    if req.method=='GET':
+        token, decoded = CheckToken(req)
+        user = User.objects.filter(username=decoded['username']).first()
+        assets = Asset.objects.filter(entity=user.entity, owner=user.username)
+        return_data = {
+            "assets": [
+                return_field(asset.serialize(), ["id", "assetName", "parentName", "category", "description", 
+                                                 "position", "value", "user", "number", "state", "department", "image"])
+            for asset in assets],
+        }
+        return request_success(return_data)
+    return BAD_METHOD

commit d2391dd1743db2b2dfe055a066a9b6285c75d24a

Fix: fix bugs caused by merge(#45)

Asset/tests.py
@@ -183,6 +183,21 @@ class AttributeTests(TestCase):
     def get_asset_user_query(self):
         return self.client.get(f'/asset/user')
     
+    def get_asset_assetName(self, assetName):
+        return self.client.get(f"/asset/{assetName}")
+    
+    def get_asset_tree(self):
+        return self.client.get(f'/asset/tree')
+    
+    def get_category_is_number(self,category_name):
+        return self.client.get(f'/asset/category/{category_name}/number')
+    
+    def get_asset_query(self, type, description, attribute):
+        return self.client.get(f'/asset/query/{type}/{description}/{attribute}')
+    
+    def get_asset_user_query(self):
+        return self.client.get(f'/asset/user')
+    
     # Now start testcases. 
     def test_asset_category_add(self):
         user = User.objects.filter(username='test_user').first()
@@ -1241,3 +1256,172 @@ class AttributeTests(TestCase):
         res = self.get_asset_user_query()
         self.assertEqual(res.json()['info'], "Succeed")
         self.assertEqual(res.json()['code'], 0)
+
+    def test_get_asset_assetName(self):
+        user = User.objects.filter(username='test_user').first()
+        user.token = user.generate_token()
+        user.system_super, user.entity_super, user.asset_super = user.set_authen("asset_super")
+        user.save()
+        Token = user.token
+        c = cookies.SimpleCookie()
+        c['token'] = Token
+        self.client.cookies = c
+
+        self.assertEqual(len(Asset.objects.all()), 1)
+
+        assetName = 'computer'
+        parentName = 'ass'
+        description = 'des'
+        position = 'pos'
+        value = '1000'
+        department = 'dep'
+        number = 1
+        categoryName = 'cate'
+        image = '127.0.0.1'
+        
+        res = self.post_asset_add(assetName, parentName, description, position, 
+                                           value, department, number, categoryName, image)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(len(Asset.objects.all()), 2)
+        self.assertTrue(Asset.objects.filter(name=assetName).exists())
+
+        attributeName = "GPU"
+        department = "dep"
+        res = self.post_attribute_add(attributeName, department)
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(res.json()['info'], 'Succeed')
+
+        assetName = "computer"
+        attributeName = "GPU"
+        description = "RTX4090"
+        res = self.post_asset_attribute_add(assetName, attributeName, description)
+        self.assertEqual(res.json()['info'], "Succeed")
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(res.json()['info'], "Succeed")
+
+        assetName = 'computer'
+        res = self.get_asset_assetName(assetName)
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(res.json()['info'], "Succeed")
+        self.assertEqual(res.json()['property']['GPU'], "RTX4090")
+        self.assertEqual(res.json()['property']['assetName'], f"{assetName}")
+
+        assetName = 'asset_1'
+        res = self.get_asset_assetName(assetName)
+        self.assertEqual(res.json()['code'], 1)
+
+    def test_asset_tree(self):
+        user = User.objects.filter(username='test_user').first()
+        user.token = user.generate_token()
+        user.system_super, user.entity_super, user.asset_super = user.set_authen("entity_super")
+        user.save()
+        Token = user.token
+        c = cookies.SimpleCookie()
+        c['token'] = Token
+        self.client.cookies = c
+
+        res = self.get_asset_tree()
+        self.assertEqual(res.json()['info'], "Succeed")
+
+    def test_category_is_number(self):
+        user = User.objects.filter(username='test_user').first()
+        user.token = user.generate_token()
+        user.system_super, user.entity_super, user.asset_super = user.set_authen("entity_super")
+        user.save()
+        Token = user.token
+        c = cookies.SimpleCookie()
+        c['token'] = Token
+        self.client.cookies = c
+        category_name = 'cate'
+
+        res = self.get_category_is_number(category_name)
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(res.json()['info'], "Succeed")
+
+        category_name = 'c_1'
+        res = self.get_category_is_number(category_name)
+        self.assertEqual(res.json()['code'], 1)
+        self.assertEqual(res.json()['info'], "不存在此资产")
+
+    def test_asset_query(self):
+        user = User.objects.filter(username='test_user').first()
+        user.token = user.generate_token()
+        user.system_super, user.entity_super, user.asset_super = user.set_authen("entity_super")
+        user.save()
+        Token = user.token
+        c = cookies.SimpleCookie()
+        c['token'] = Token
+        self.client.cookies = c
+        
+        # type = "asset_name"
+        type = "asset_name"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(res.json()['info'], 'Succeed')
+
+        # type = "asset_description"
+        type = "asset_description"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['code'], 0)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        
+        # type = "asset_position"
+        type = "asset_position"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        self.assertEqual(res.json()['code'], 0)
+
+        # type = "asset_type"
+        type = "asset_type"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        self.assertEqual(res.json()['code'], 0)
+
+        # type = "asset_status"
+        type = "asset_status"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        self.assertEqual(res.json()['code'], 0)
+        
+        # type = "asset_department"
+        type = "asset_department"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        self.assertEqual(res.json()['code'], 0)
+
+        # type = "asset_department"
+        type = "asset_owner"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['info'], 'Succeed')
+        self.assertEqual(res.json()['code'], 0)
+
+        # error_type
+        type = "department"
+        description = "a"
+        attribute = "1"
+
+        res = self.get_asset_query(type, description, attribute)
+        self.assertEqual(res.json()['code'], 1)
+        self.assertEqual(res.json()['info'], '此搜索类型不存在')