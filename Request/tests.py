27bc32bc28ec8a7cf7f66f975ff873fd68d87295
Camellia_TT <wangchuy21@mails.tsinghua.edu.cn>
2023-04-22 10:50:50 +0800

    Feature: add urls and imports of Request

--- Request/tests.py
+++ Request/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from Asset.models import AssetCategory, Asset, Attribute, AssetAttribute
+from Department.models import Department, Entity
+from User.models import User, Menu
+import hashlib
+from http import cookies
 
 # Create your tests here.
+
+# Test for Attribute
+class AttributeTests(TestCase):
+    def test_for_unit(self):
+        self.assertNotEqual(0, 1)
+
+    # Initializer
+    def setUp(self):
+        ent = Entity.objects.create(id=1, name='ent')
+        
+        dep_ent = Department.objects.create(id=1, name='ent', entity=ent)
+        dep = Department.objects.create(id=2, name='dep', entity=ent)
+        dep_child = Department.objects.create(id=3, name='dep_child', entity=ent, parent=dep)
+        
+        password='123'
+        md5 = hashlib.md5()
+        md5.update(password.encode('utf-8'))
+        pwd = md5.hexdigest()
+
+        User.objects.create(username='Alice', password=pwd, department=dep_child, entity=ent)
+        User.objects.create(username='George', password=pwd, department=dep, entity=ent, asset_super=True)
+        User.objects.create(username='test_attribute', password=pwd, department=dep, entity=ent)
+        User.objects.create(username='test_user', password=pwd, department=dep, entity=ent)
+        
+        category = AssetCategory.objects.create(name='cate', entity=ent)
+        
+        Asset.objects.create(name='ass', entity=ent, owner="test_user", category=category, department=dep)
+        Attribute.objects.create(id=1, name="attri_0", entity=ent, department=dep_ent)
+        
--- Request/views.py
+++ Request/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import json
+from django.http import HttpRequest, HttpResponse
+
+from utils.utils_request import BAD_METHOD, request_failed, request_success, return_field
+from utils.utils_require import MAX_CHAR_LENGTH, CheckRequire, require
+from utils.utils_time import get_timestamp
+from utils.utils_getbody import get_args
+from utils.utils_checklength import checklength
+from utils.utils_checkauthority import CheckAuthority, CheckToken
+
+from User.models import User, Menu
+from Department.models import Department, Entity
+from Asset.models import Attribute, Asset, AssetAttribute, AssetCategory
+
+from eam_backend.settings import SECRET_KEY
+import jwt
 
 # Create your views here.
