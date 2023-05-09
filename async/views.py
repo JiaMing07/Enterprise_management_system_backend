0e040ebfaf4204021a037f0301f3bb5f291596d8
沈佳茗 <2021010745@secoder.net>
2023-05-09 18:28:00 +0800

    Feat: use async views for assets add

--- async/views.py
+++ async/views.py
@@ -25,6 +25,7 @@ from eam_backend.settings import SECRET_KEY
 import jwt
 import asyncio
 from asgiref.sync import sync_to_async
+import datetime
 
 from .models import AsyncModel, AsyncTask
 
@@ -62,88 +63,103 @@ async def http_call_async(i):
     except Exception as e:
         print(e)
 
-async def add(assets_new, username):
-    asy = await AsyncModel.objects.acreate(initiator="Alice", start_time=get_date(), status = "STARTED", body = {'assets_new': assets_new})
+async def add_asset(assets_new, username):
     user = await User.objects.filter(username=username).afirst()
-    entity=user.entity
+    entity = await Entity.objects.filter(id=user.entity_id).afirst()
+    asy = await AsyncModel.objects.acreate(initiator=username, start_time=get_date(), status = "STARTED", body = {'assets_new': assets_new}, entity=entity)
     err_msg=""
     for idx, asset_single in enumerate(assets_new):
-        name, parentName, description, position, value, department, number, categoryName, image_url = get_args(
-        asset_single, ["name", "parent", "description", "position", "value", "department", "number", "category", "image"], 
-        ["string", "string", "string", "string", "int", "string", "int", "string", "string"])
-        state = asset_single.get('state', 'IDLE')
-        owner = asset_single.get('owner', "")
-        def check_length(string, lowerbound, upperbound, name,err_msg):
-            if lowerbound < len(string) <=upperbound:
-                err_msg += f"Bad length of [{name}]；"
-        check_length(name, 0, 50, "assetName", err_msg)
-        check_length(parentName, -1, 50, "parentName", err_msg)
-        check_length(department, -1, 30, "department", err_msg)
-        check_length(categoryName, 0, 50, "categoryName", err_msg)
-        check_length(description, 0, 300, "description", err_msg)
-        check_length(position, 0, 300, "position", err_msg)
-        check_length(image_url, -1, 300, "imageURL", err_msg)
-        if parentName == "":
-            parentName = entity.name
-            parent = await Asset.objects.filter(name=entity.name).afirst()
-            if parent is None:
-                d = await Department.objects.filter(entity=entity, name=entity.name).afirst()
-                c = await AssetCategory.objects.filter(id=1).afirst()
-                p = await Asset.objects.filter(id=1).afirst()
-                parent = Asset(name=entity.name, owner=user.username, 
-                            category=c, entity=entity, department=d, parent=p)
-                parent.asave()
-            if department == "":
-                department = user.department
-            else:
-                department = await Department.objects.filter(entity=entity, name=department).afirst()
-                if department is None:
-                    err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，挂账部门不存在" + '；'
-        else:
-            parent = await Asset.objects.filter(entity=entity, name=parentName).afirst()
-            if parent is None:
-                err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，父资产不存在" + '；'
+        try:
+            try:
+                name, parentName, description, position, value, department, number, categoryName, image_url = get_args(
+                asset_single, ["name", "parent", "description", "position", "value", "department", "number", "category", "image"], 
+                ["string", "string", "string", "string", "int", "string", "int", "string", "string"])
+            except Exception as e:
+                error_code = -2 if len(e.args) < 2 else e.args[1]
+                err_msg = err_msg + '第' +str(idx + 1) +"条资产输入信息有误, " + e.args[0] + ";"  # Refer to below
+                print(err_msg)
                 continue
-            if department == "":
-                department = parent.department
+            print(name, parentName, description, position, value, department, number, categoryName, image_url)
+            state = asset_single.get('state', 'IDLE')
+            owner = asset_single.get('owner', "")
+            def check_length(string, lowerbound, upperbound, name,err_msg):
+                if lowerbound < len(string) <=upperbound:
+                    err_msg += f"Bad length of [{name}]；"
+            check_length(name, 0, 50, "assetName", err_msg)
+            check_length(parentName, -1, 50, "parentName", err_msg)
+            check_length(department, -1, 30, "department", err_msg)
+            check_length(categoryName, 0, 50, "categoryName", err_msg)
+            check_length(description, 0, 300, "description", err_msg)
+            check_length(position, 0, 300, "position", err_msg)
+            check_length(image_url, -1, 300, "imageURL", err_msg)
+            if parentName == "":
+                parentName = entity.name
+                parent = await Asset.objects.filter(name=entity.name).afirst()
+                if parent is None:
+                    d = await Department.objects.filter(entity=entity, name=entity.name).afirst()
+                    c = await AssetCategory.objects.filter(id=1).afirst()
+                    p = await Asset.objects.filter(id=1).afirst()
+                    parent = Asset(name=entity.name, owner=user.username, 
+                                category=c, entity=entity, department=d, parent=p)
+                    parent.asave()
+                if department == "":
+                    department = Department.objects.filter(id=user.department_id).afirst()
+                else:
+                    department = await Department.objects.filter(entity=entity, name=department).afirst()
+                    if department is None:
+                        err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，挂账部门不存在" + '；'
             else:
-                department = await Department.objects.filter(entity=entity, name=department).afirst()
-                if department is None:
-                    err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，挂账部门不存在" + '；'
+                parent = await Asset.objects.filter(entity=entity, name=parentName).afirst()
+                if parent is None:
+                    err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，父资产不存在" + '；'
                     continue
-        category = await AssetCategory.objects.filter(name=categoryName, entity=entity).afirst()
-        if category is None:
-            err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，资产类型不存在" + '；'
-            continue
-        asset = await Asset.objects.filter(entity=entity, name=name).afirst()
-        if asset:
-            err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，该资产已存在" + '；'
-            continue
-        if owner == "":
-            owner = await User.objects.filter(entity=entity, department=department, asset_super=True).afirst()
-            if owner is None:
-                err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，部门不存在资产管理员" + '；'
+                if department == "":
+                    department = Department.objects.filter(id=parent.department_id).afirst()
+                else:
+                    department = await Department.objects.filter(entity=entity, name=department).afirst()
+                    if department is None:
+                        err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，挂账部门不存在" + '；'
+                        continue
+            category = await AssetCategory.objects.filter(name=categoryName, entity=entity).afirst()
+            if category is None:
+                err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，资产类型不存在" + '；'
                 continue
-        else:
-            owner = await User.objects.filter(entity=entity, department=department, username=owner).afirst()
-            if owner is None:
-                err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，挂账人不存在" + '；'
+            asset = await Asset.objects.filter(entity=entity, name=name).afirst()
+            if asset:
+                err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，该资产已存在" + '；'
                 continue
-        ancestor_list = department.get_ancestors(include_self=True)
-        flag = False
-        async for ancestor in ancestor_list:
-            if user.department.id == ancestor.id:
-                flag = True
-                break
-        if flag == False:
-            err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，部门不在管理范围内" + '；'
+            if owner == "":
+                owner = await User.objects.filter(entity=entity, department=department, asset_super=True).afirst()
+                if owner is None:
+                    err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，部门不存在资产管理员" + '；'
+                    continue
+            else:
+                owner = await User.objects.filter(entity=entity, department=department, username=owner).afirst()
+                if owner is None:
+                    err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，挂账人不存在" + '；'
+                    continue
+            ancestor_list = department.get_ancestors(include_self=True)
+            flag = False
+            async for ancestor in ancestor_list:
+                if user.department_id == ancestor.id:
+                    flag = True
+                    break
+            if flag == False:
+                err_msg = err_msg +'第' +str(idx + 1) +"条资产录入失败，部门不在管理范围内" + '；'
+                continue
+            late = await Asset.objects.all().order_by('id').alast()
+            owner_name = owner.username
+            asset = Asset(name=name, description=description, position=position, value=value, owner=owner_name, number=number,
+                        category=category, entity=entity, department=department, parent=parent, image_url=image_url,state=state)
+            await asset.asave()
+        except Exception as e:
+            print(e)
             continue
-        await Asset.objects.acreate(name=name, description=description, position=position, value=value, owner=owner.username, number=number,
-                    category=category, entity=entity, department=department, parent=parent, image_url=image_url,state=state)
     asy.end_time = get_date()
     asy.status = 'SUCCESS'
     asy.result = 'ok'
-    print('ok')
+    if len(err_msg) > 0:
+        asy.result = err_msg[:-1]
     await asy.asave()
 
 async def test2(req: HttpRequest):
@@ -158,7 +174,11 @@ async def test2(req: HttpRequest):
 @CheckRequire
 def model_list(req: HttpRequest):
     if req.method == 'GET':
-        models = AsyncModel.objects.all()
+        CheckAuthority(req, ['entity_super'])
+        token, decoded = CheckToken(req)
+        user = User.objects.filter(username=decoded['username']).first()
+        entity = user.entity
+        models = AsyncModel.objects.filter(entity=entity)
         print(models)
         data = []
         for m in models:
@@ -168,15 +188,41 @@ def model_list(req: HttpRequest):
         })
     return BAD_METHOD
 
-@CheckRequire
-def add(req: HttpRequest):
+
+async def add(req: HttpRequest):
     if req.method == 'POST':
-        CheckAuthority(req, ["entity_super", "asset_super"])
+        print(req.COOKIES)
+        # CheckAuthority(req, ["entity_super", "asset_super"])
+        try:
+            token = req.COOKIES['token']
+        except KeyError:
+            return request_failed(-2, 'Token未给出',status_code=400)
+        try:
+            decoded = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            decode = jwt.decode(token, SECRET_KEY, leeway=datetime.timedelta(days=3650), algorithms=['HS256'])
+            user = await User.objects.filter(username=decode['username']).afirst()
+            user.token = ''
+            user.asave()
+            return request_failed(-2, 'Token已过期',status_code=400)
+        except jwt.InvalidTokenError:
+            return request_failed(-2, 'Token不合法',status_code=400)
+        user = await User.objects.filter(username=decoded['username']).afirst()
+        if user is None:
+            return request_failed(-2, "不存在对应的用户",status_code=400)
+        if user.token != token:
+            return request_failed(-2, "用户已在其他地方登录，请退出后重新登陆",status_code=400)
+        have_authority = False
+        for au in ['entity_super', 'asset_super']:
+            if user.check_authen() == au:
+                have_authority = True
+        if have_authority == False:        
+            return request_failed(-2, "没有操作权限",status_code=400)
         body = json.loads(req.body.decode("utf-8"))
         assets_new = body['assets']
         token, decoded = CheckToken(req)
-        user = User.objects.filter(username=decoded['username']).first()
-        res = add_assets.delay(assets_new, user.username)
+        loop = asyncio.get_event_loop()
+        loop.create_task(add_asset(assets_new, user.username))
         return request_success()
     return BAD_METHOD
 
