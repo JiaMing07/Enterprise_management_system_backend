83f1c4b723b49746e577c10d36b1ad27b45e378a
沈佳茗 <2021010745@secoder.net>
2023-05-24 02:38:22 +0800

    Fix: add uwsgi

--- start.sh
+++ start.sh
@@ -9,13 +9,13 @@ python3 manage.py migrate
 
 # TODO Start: [Student] Run with uWSGI instead
 # python3 manage.py runserver 8000
-# uwsgi -d --module=eam_backend.wsgi:application \
-#     --env DJANGO_SETTINGS_MODULE=eam_backend.settings \
-#     --master \
-#     --http=0.0.0.0:80 \
-#     --processes=5 \
-#     --harakiri=1200 \
-#     --max-requests=5000 \
-#     --vacuum &&
-daphne -b 0.0.0.0 -p 80 --application-close-timeout 1200 eam_backend.asgi:application
+uwsgi -d --module=eam_backend.wsgi:application \
+    --env DJANGO_SETTINGS_MODULE=eam_backend.settings \
+    --master \
+    --http=0.0.0.0:80 \
+    --processes=5 \
+    --harakiri=300 \
+    --max-requests=5000 \
+    --vacuum &&
+daphne -b 0.0.0.0 -p 80 --application-close-timeout 300 eam_backend.asgi:application
 # TODO End: [Student] Run with uWSGI instead
